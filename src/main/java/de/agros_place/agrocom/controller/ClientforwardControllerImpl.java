ea3c3de7bff45c3b72e51b1cfc9e98d8705a266c
Daniel Agro <dagro@timocom.com>
2022-05-16 09:53:40 +0200

    ignore ClientforwardController from swagger documentation

--- src/main/java/de/agros_place/agrocom/controller/ClientforwardControllerImpl.java
+++ src/main/java/de/agros_place/agrocom/controller/ClientforwardControllerImpl.java
@@ -1,10 +1,13 @@
 package de.agros_place.agrocom.controller;
 
+import springfox.documentation.annotations.ApiIgnore;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ClientforwardControllerImpl {
+  @ApiIgnore
   @GetMapping(value = "/**/{path:[^.]*}")
   public String forward() {
     return "forward:/";
